792b41b76692f268ba4e0f399c0aa376e0741204
ganganiangao <ray1019c@gmail.com>
2025-06-14 00:00:01 +0800

    add question 070

---
+++ 070_climbing_stairs/070_climbing_stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> val;
+    Solution() : val(46, 0) {
+        val[0] = 0;
+        val[1] = 1;
+        val[2] = 2;
+    }
+
+    int climbStairs(int n) {  
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        if(val[n]) return val[n];
+        return val[n] = climbStairs(n-1) + climbStairs(n-2);        
+    }
+};
